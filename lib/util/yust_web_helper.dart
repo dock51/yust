795003c27459b8ab8d78911f1f2dc89d07c88749
Jannes Koehler <janneskoehler@gmail.com>
2020-09-24 12:17:05 +0200

    Implement web helper

--- lib/util/yust_web_helper.dart
+++ lib/util/yust_web_helper.dart
@@ -1,15 +1,31 @@
 import 'dart:typed_data';
 
-import 'package:yust/util/yust_exception.dart';
+import 'package:firebase/firebase.dart' as fb;
+import 'package:mime/mime.dart';
+
+import '../yust.dart';
 
 class YustWebHelper {
   static Future<String> uploadImage(
       String path, String name, Uint8List bytes) async {
-    YustException('Function not implemented');
-    return null;
+    var metadata = fb.UploadMetadata(
+      contentType: lookupMimeType(name),
+    );
+    fb.StorageReference ref =
+        fb.app().storage().refFromURL(Yust.storageUrl).child(path).child(name);
+    fb.UploadTask uploadTask = ref.put(bytes, metadata);
+    fb.UploadTaskSnapshot taskSnapshot = await uploadTask.future;
+    final uri = await taskSnapshot.ref.getDownloadURL();
+    return uri.toString();
   }
 
   static Future<void> deleteImage(String path, String name) async {
-    YustException('Function not implemented');
+    await fb
+        .app()
+        .storage()
+        .refFromURL(Yust.storageUrl)
+        .child(path)
+        .child(name)
+        .delete();
   }
 }
